58201d3514a34a3c48c69b0e85f66c81e484a3fe
YanaGuzienko <69dusha69@ukr.net>
2021-04-25 17:57:31 +0300

    добавляет стили

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -3,14 +3,26 @@ import scss from './Statistics.module.scss';
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <>
-      {positivePercentage > 0 ? (
+      {good > 0 || neutral > 0 || bad > 0 ? (
         <>
           <h1 className={scss.title}>Statistic</h1>
-          <p>Good:{good}</p>
-          <p>Neutral:{neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>Total: {total}</p>
-          <p>Positive feedback: {positivePercentage}%</p>
+          <ul className={scss.list}>
+            <li className={scss.item}>
+              <p className={scss.text}>Good:{good}</p>
+            </li>
+            <li className={scss.item}>
+              <p className={scss.text}>Neutral:{neutral}</p>
+            </li>
+            <li className={scss.item}>
+              <p className={scss.text}>Bad: {bad}</p>
+            </li>
+            <li className={scss.item}>
+              <p className={scss.text}>Total: {total}</p>
+            </li>
+            <li className={scss.item}>
+              <p className={scss.text}>Positive feedback: {positivePercentage}%</p>
+            </li>
+          </ul>
         </>
       ) : (
         <h2>No feedback given</h2>
